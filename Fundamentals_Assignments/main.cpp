54f2baef92453e0a6980a36733860ca10e0a7171
Ahmed Badawi <ahmedbadawi.1411@gmail.com>
2023-12-03 10:52:44 -0800

    Program 4

--- Fundamentals_Assignments/main.cpp
+++ Fundamentals_Assignments/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 void print();
 void basic_arithmatic_operations();
 void sum();
+void solve_equations();
 
 int main()
 {
-    sum();
+    solve_equations();
     return 0;
 }
 
@@ -47,5 +49,29 @@ void sum()
     cout<<"Third Number  : ";
     cin>>num3;
     cout<<num1<<"  +  " <<num2<<"  +  " <<num3<<" = "<<num1+num2+num3<<endl;
+}
+
+void solve_equations()
+{
+    short choose;
+    float x,result;
+    cout<<"\n1: f(x) = x^2 + 7x - 9\n2: g(x) = x^6 + 5x^4 + 2x^3 + 25\n";
+    cout<<"\nChoose An Equation : ";
+    cin>>choose;
+    if(choose == 1)
+    {
+         cout<<"Input x value: ";
+         cin>>x;
+         result = ((x*x) + (7*x)) -9;
+         cout<<"\nf("<<x<<")= "<<x<<"^2+7*"<<x<<"-9 = "<<result;
+    }
+    else if(choose == 2)
+    {
+        cout<<"Input x value: ";
+        cin>>x;
+        result = (pow(x,6)) + (5*(pow(x,4))) +(2*(pow(x,3))) + 25;
+        cout<<"\nf("<<x<<")= "<<x<<"^6+5"<<x<<"^4"<<"2"<<x<<"^3"<<"+25 = "<<result<<endl;
+    }
+
 }
 
