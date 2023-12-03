de4e728e90d313280b608b9ce5693f5055c9f297
Ahmed Badawi <ahmedbadawi.1411@gmail.com>
2023-12-03 09:08:12 -0800

    Program 1

--- Fundamentals_Assignments/main.cpp
+++ Fundamentals_Assignments/main.cpp
@@ -2,12 +2,23 @@
 
 using namespace std;
 
+void print();
+
 int main()
 {
-
+    print();
     return 0;
 }
 
 
+void print()
+{
+    cout<<"************************\n";
+    cout<<"Programming Fundamentals\n";
+    cout<<"'C++'\n";
+    cout<<"Basic Concepts\n";
+    cout<<"\\\\\\\\Let's Start\\\\\\\\\n";
+    cout<<"************************\n";
+}
 
 
